ef47ecca09fb80519c59e2483ff63a7f8cc42328
HactuallyBenji <38388426+HactuallyBenji@users.noreply.github.com>
2020-05-25 12:06:53 -0700

    Index done

--- main_script.js
+++ main_script.js
@@ -19,10 +19,12 @@ function normalize_ham() {
 
 function togglemyNav(){
     if(document.getElementById("myNav").style.width == "30%"){
-        document.getElementById("myNav").style.width = "0%";
-        document.getElementById("myNav").style.height = "0%";
 
+        document.getElementById("myNav").style.width = "0%";
+        document.getElementById("black").style.width = "100%";
+        $("#black").fadeOut(300);
         
+
         /*
       document.getElementById("header").style.backgroundColor = "rgba(94,16,10)";
         document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
@@ -30,10 +32,11 @@ function togglemyNav(){
         
         
     }else{
+        document.getElementById("myNav").style.zIndex = "0";
         document.getElementById("myNav").style.width = "30%";
-        document.getElementById("myNav").style.height = "100%";
-
-
+        $("#black").fadeIn(300);
+        document.getElementById("black").style.width = "70%";
+        
         /*
         document.getElementById("header").style.backgroundColor = "transparent";
         document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0), 0 6px 20px 0 rgba(0, 0, 0, 0)";
@@ -44,9 +47,23 @@ function togglemyNav(){
 
 
 function decreaseNav() {
-        document.getElementById("myNav").style.width = "0%";
-        document.getElementById("myNav").style.height = "0%";
+        //document.getElementById("myNav").style.width = "0%";
+        //document.getElementById("myNav").style.height = "0%";
         document.getElementById("navButtonBars").classList.toggle("change", false);
 }
 
+window.onscroll = function() {fade(element)};
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
 

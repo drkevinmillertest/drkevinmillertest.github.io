2ecc709696a01efa8a811b1599b9b06b2aa1af81
HactuallyBenji <38388426+HactuallyBenji@users.noreply.github.com>
2020-05-30 11:14:53 -0700

    Final stages of website

--- main_script.js
+++ main_script.js
@@ -5,50 +5,73 @@ function animation(x){
 
 
 function change_ham_color() {
-    document.getElementById("topbar").style.backgroundColor = "#ad1b10";
-    document.getElementById("midbar").style.backgroundColor = "#ad1b10";
-    document.getElementById("botbar").style.backgroundColor = "#ad1b10";
+    document.getElementById("topbar").style.backgroundColor = "#8c0000";
+    document.getElementById("midbar").style.backgroundColor = "#8c0000";
+    document.getElementById("botbar").style.backgroundColor = "#8c0000";
 }
 
 function normalize_ham() {
-    document.getElementById("topbar").style.backgroundColor = "darkslategray";
-    document.getElementById("midbar").style.backgroundColor = "darkslategray";
-    document.getElementById("botbar").style.backgroundColor = "darkslategray";
+    document.getElementById("topbar").style.backgroundColor = "#525c63";
+    document.getElementById("midbar").style.backgroundColor = "#525c63";
+    document.getElementById("botbar").style.backgroundColor = "#525c63";
 }
 
 
 function togglemyNav(){
-    if(document.getElementById("myNav").style.width == "30%"){
+    
+    
+    if(screen.availHeight > screen.availWidth) {
+        
+        if(document.getElementById("myNav").style.width == "70%"){
 
         document.getElementById("myNav").style.width = "0%";
         document.getElementById("black").style.width = "100%";
-        $("#black").fadeOut(300);
-        
-
+        $("#black").fadeOut(400);
         /*
       document.getElementById("header").style.backgroundColor = "rgba(94,16,10)";
         document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
         */
         
-        
     }else{
-        document.getElementById("myNav").style.zIndex = "0";
-        document.getElementById("myNav").style.width = "30%";
+
+        document.getElementById("myNav").style.width = "70%";
         $("#black").fadeIn(300);
-        document.getElementById("black").style.width = "70%";
-        
+        document.getElementById("black").style.width = "30%";
         /*
         document.getElementById("header").style.backgroundColor = "transparent";
         document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0), 0 6px 20px 0 rgba(0, 0, 0, 0)";
-        
         */
+    }
+        
+    }else {
+        if(document.getElementById("myNav").style.width == "30%"){
+
+            document.getElementById("myNav").style.width = "0%";
+            document.getElementById("black").style.width = "100%";
+            $("#black").fadeOut(400);
+            /*
+          document.getElementById("header").style.backgroundColor = "rgba(94,16,10)";
+            document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+            */
+
+        }else{
+
+            document.getElementById("myNav").style.width = "30%";
+            $("#black").fadeIn(300);
+            document.getElementById("black").style.width = "70%";
+            /*
+            document.getElementById("header").style.backgroundColor = "transparent";
+            document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0), 0 6px 20px 0 rgba(0, 0, 0, 0)";
+            */
+        }
     }
 }
 
 
 function decreaseNav() {
-        //document.getElementById("myNav").style.width = "0%";
-        //document.getElementById("myNav").style.height = "0%";
+        document.getElementById("myNav").style.width = "0%";
+        document.getElementById("black").style.width = "100%";
+        $("#black").fadeOut(400);
         document.getElementById("navButtonBars").classList.toggle("change", false);
 }
 
